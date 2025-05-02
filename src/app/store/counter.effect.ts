9a8b26044fd20070c1de229e4355e83d923cb0d5
adnan5509 <adnanafzal5509@gmail.com>
2025-05-02 15:55:48 +0200

    refactor CounterEffects to save current count in localStorage on increment and decrement actions

--- src/app/store/counter.effect.ts
+++ src/app/store/counter.effect.ts
@@ -1,18 +1,21 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
 import { decrement, increment } from "./counter.actions";
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.selectors";
 
 @Injectable()
 export class CounterEffects {
-    constructor(private actions$: Actions) { }
+    constructor(private actions$: Actions, private store: Store<{ counter: number }>) { }
 
     saveCount = createEffect(
         () =>
             this.actions$.pipe(
                 ofType(increment, decrement),
-                tap((action) => {
-                    localStorage.setItem('Change', action.value.toString());
+                withLatestFrom(this.store.select(selectCount)),
+                tap(([action, counter]) => {
+                    localStorage.setItem('Change', counter.toString());
                 })
             ), { dispatch: false }
     )
