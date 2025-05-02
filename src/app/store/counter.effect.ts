de73fb8d9eb726d1455975dc6e0cc82ee6f600cb
adnan5509 <adnanafzal5509@gmail.com>
2025-05-02 21:43:33 +0200

    implement initialization of counter state from localStorage and add set action to counter

--- src/app/store/counter.effect.ts
+++ src/app/store/counter.effect.ts
@@ -1,6 +1,6 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { tap, withLatestFrom } from "rxjs";
-import { decrement, increment } from "./counter.actions";
+import { of, switchMap, tap, withLatestFrom } from "rxjs";
+import { decrement, increment, init, set } from "./counter.actions";
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { selectCount } from "./counter.selectors";
@@ -9,13 +9,29 @@ import { selectCount } from "./counter.selectors";
 export class CounterEffects {
     constructor(private actions$: Actions, private store: Store<{ counter: number }>) { }
 
+    loadCount = createEffect(
+        () => this.actions$.pipe(
+            ofType(init),
+            switchMap(
+                () => {
+                    const savedCounter = localStorage.getItem('count');
+                    if (savedCounter) {
+                        return of(set({ value: +savedCounter }))
+                    } else {
+                        return of(set({ value: 0 }))
+                    }
+                }
+            )
+        )
+    )
+
     saveCount = createEffect(
         () =>
             this.actions$.pipe(
                 ofType(increment, decrement),
                 withLatestFrom(this.store.select(selectCount)),
                 tap(([action, counter]) => {
-                    localStorage.setItem('Change', counter.toString());
+                    localStorage.setItem('count', counter.toString());
                 })
             ), { dispatch: false }
     )
